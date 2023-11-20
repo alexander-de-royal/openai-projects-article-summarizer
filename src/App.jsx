c31c651380b352409a6789e0f5805f40dbeec90f
Alexander Royal <alexander.de.royal@gmail.com>
2023-11-20 15:52:41 +0545

    hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import {Hero} from './components/hero';
+import {Demo} from './components/demo';
+import "./App.css"
 
 export const App = () => {
     return (
-        <div>App</div>
-    )
+        <main>
+            <div className="main">
+                <div className="gradient"/>
+            </div>
+            <div className="app">
+                <Hero/>
+                <Demo/>
+            </div>
+        </main>
+    );
 }
+
+
