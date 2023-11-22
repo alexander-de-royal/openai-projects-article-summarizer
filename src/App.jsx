ac2669b93781784e86804bc60382886f43f5dff3
Alexander Royal <alexander.de.royal@gmail.com>
2023-11-22 10:29:54 +0545

    .env file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import {Hero} from './components/hero';
-import {Demo} from './components/demo';
-import "./App.css"
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
 
-export const App = () => {
+import "./App.css";
+
+const App = () => {
     return (
         <main>
-            <div className="main">
-                <div className="gradient"/>
+            <div className='main'>
+                <div className='gradient' />
             </div>
-            <div className="app">
-                <Hero/>
-                <Demo/>
+
+            <div className='app'>
+                <Hero />
+                <Demo />
             </div>
         </main>
     );
-}
+};
 
-
+export default App;
